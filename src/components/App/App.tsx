02208c779e685337007dc6ecd29ac4238eb6411a
Roberto Scolar <robertobs1@hotmail.com>
2022-04-10 12:33:34 -0300

    propify table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,14 @@ import Header from '../Header';
 import './App.css';
 import Container from '../../shared/Container';
 import Table from '../../shared/Table';
+import { TableHeader } from '../../shared/Table';
+import Products from '../../shared/Table/Table.mockdata';
+
+const headers: TableHeader[] = [
+  { key:'name', value: 'Product'},
+  { key:'price', value: 'Price', right: true},
+  { key:'stock', value: 'Available Stock', right: true}
+]
 
 function App() {
 
@@ -11,7 +19,8 @@ function App() {
       <Header title='AlgaStock'/>
       <Container>    
         <Table
-              
+            headers={headers}
+            data={Products}
         />
       </Container>
     </div>
