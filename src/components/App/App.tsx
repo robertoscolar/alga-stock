2ff2b49bf95089c698e0ed4b0fa31f244981d02a
Roberto Scolar <robertobs1@hotmail.com>
2022-04-10 00:08:06 -0300

    clear app + add table components base files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../Header';
 import './App.css';
-import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
-
 
 function App() {
-  const [street, setStreet] = useState('');
 
   return (
     <div className="App">
       <Header title='AlgaStock'/>
       <Container>    
-        <ul>
-          {
-            ['Daniel', 'Bruno', 'Roberto'].map((name, index) => {
-              return <li key={index}>
-                {name}
-              </li>
-            }) 
-          }
-        </ul>
+        
       </Container>
     </div>
   );
