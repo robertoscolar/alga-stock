5248aaa7ee09ef84ea15ef11789e1a8db6b0aac5
Roberto Scolar <robertobs1@hotmail.com>
2022-04-12 18:41:44 -0300

    add table actions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -46,6 +46,10 @@ function App() {
         <Table
             headers={headers}
             data={products}
+            enableActions={true}
+            onDelete={console.log}
+            onDetail={console.log}
+            onEdit={console.log}
         />
 
         <ProductForm 
